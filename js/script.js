b1d15972cb625a724dd5c49b1ca7b0cfa5054c38
Vasiliy <Spring.wind.of.change@gmail.com>
2022-01-18 16:33:28 +0200

    first comit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+" use strict ";
+
+// види змінних
+console.log(leftBorderWidth);
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+console.log(second);
+console.log(pi);
+
+
+let something;
+console.log(something);
+
+
+// obuect
+let mas = {
+    name: "Jon",
+    age: 18,
+    isMaried: true
+};
+console.log(mas);
+console.log(mas.age);
+console.log(mas["name"]);
+
+
+// masiv
+let arr = ['bob', 'fog', 'nom'];
+
+console.log(arr);
+console.log(arr[0]);
+
